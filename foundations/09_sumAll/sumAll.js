bedddcc0da9528d67f084854c0049667c2cefff0
leo <187560246+Leodata01@users.noreply.github.com>
2026-01-31 16:01:40 +0900

    finish ex 09 sumALL

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+  if (int1 < 0 || int2 < 0) return "ERROR";
+  if (!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
 
+  let min = int2;
+  let max = int1;
+
+  console.log(min);
+  // assign min and max
+  if (int1 < int2) {
+    min = int1;
+    max = int2;
+  }
+  console.log(min);
+
+  // create variable list int
+  let list = 0;
+  // loop between min and max every number and add the int to the list
+
+  for (i = min; i <= max; i++) {
+    list += i;
+  }
+
+  return list;
 };
+
+console.log(sumAll(2, 3));
 
 // Do not edit below this line
 module.exports = sumAll;
