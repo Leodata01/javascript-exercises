c79d4348c49c5848232bcd800e60a8c1024d08bb
leo <187560246+Leodata01@users.noreply.github.com>
2026-01-31 14:15:23 +0900

    finish exo 8 remove..

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr = []) {
+  // loop through every arguments but the first one
+  for (argument = 1; argument < arguments.length; argument++) {
+    // loop through the arr to remove the argument
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === arguments[argument]) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return arr;
 };
+
+console.log(removeFromArray([1, 2, 2, 2, 2, 3], 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
