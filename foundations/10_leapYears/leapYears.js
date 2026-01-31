5dc483044dc811221b02b3b8466b0911d2b04342
leo <187560246+Leodata01@users.noreply.github.com>
2026-01-31 16:58:31 +0900

    finish ex 10 leapyears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,29 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let isLeapYear = false;
 
+  let isDivisible4 = false;
+  let isDivisible100 = false;
+  let isDivisible400 = false;
+
+  if (year % 4 === 0) isDivisible4 = true;
+  if (year % 100 === 0) isDivisible100 = true;
+  if (year % 400 === 0) isDivisible400 = true;
+
+  if (isDivisible4 && !isDivisible100) {
+    isLeapYear = true;
+    return isLeapYear;
+  }
+
+  if (isDivisible400) {
+    isLeapYear = true;
+    return isLeapYear;
+  }
+
+  return isLeapYear;
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
