3671c7536a25f563c746ea23326ce461f8d47ad8
leo <187560246+Leodata01@users.noreply.github.com>
2026-01-31 12:49:04 +0900

    finish exo 7 reversestring

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (value) {
+  let reverse = "";
+  for (i = value.length; i >= 0; i--) {
+    reverse += value.slice(i, i + 1);
+  }
+  return reverse;
 };
 
 // Do not edit below this line
