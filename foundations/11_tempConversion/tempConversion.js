816d7113d7175c03fafd8c3382fea2a7a57f3c77
leo <187560246+Leodata01@users.noreply.github.com>
2026-01-31 17:30:06 +0900

    finish ex 11 temp

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temparature) {
+  if (typeof temparature !== Number) "ERROR";
+
+  const celsius = ((temparature - 32) * 5) / 9;
+  console.log(celsius);
+
+  const roundCel = Math.round(celsius * 10) / 10;
+  return roundCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temparature) {
+  if (typeof temparature !== Number) "ERROR";
+
+  const fahrenheit = (temparature * 9) / 5 + 32;
+  const roundfahr = Math.round(fahrenheit * 10) / 10;
+  return roundfahr;
 };
+
+console.log(convertToCelsius(-100));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
