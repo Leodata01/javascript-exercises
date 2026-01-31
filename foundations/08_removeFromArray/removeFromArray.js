6a1255937e71a31c0c925f8bb71b7f5391ff58a8
leo <187560246+Leodata01@users.noreply.github.com>
2026-01-31 18:06:49 +0900

    redo the ex 08 removefromarray with filter

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,16 +1,20 @@
-const removeFromArray = function (arr = []) {
-  // loop through every arguments but the first one
-  for (argument = 1; argument < arguments.length; argument++) {
-    // loop through the arr to remove the argument
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === arguments[argument]) {
-        arr.splice(i, 1);
-        i--;
-      }
-    }
-  }
-  return arr;
-};
+// const removeFromArray = function (arr = []) {
+//   // loop through every arguments but the first one
+//   for (argument = 1; argument < arguments.length; argument++) {
+//     // loop through the arr to remove the argument
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] === arguments[argument]) {
+//         arr.splice(i, 1);
+//         i--;
+//       }
+//     }
+//   }
+//   return arr;
+// };
+
+function removeFromArray(list = [], ...otherArgs) {
+  return list.filter((value) => !otherArgs.includes(value));
+}
 
 console.log(removeFromArray([1, 2, 2, 2, 2, 3], 2));
 
