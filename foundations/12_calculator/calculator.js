600d6ab75d1e3bdedc198bf628a1423c7be5e310
leo <187560246+Leodata01@users.noreply.github.com>
2026-02-11 09:23:34 +0700

    finish ex 12 caulculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, item) => {
+    return sum + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr = []) {
+  return arr.reduce((multiply, item) => {
+    return multiply * item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let value = 1;
+  for (let i = n; i > 1; i--) {
+    value = value * i;
+  }
+  return value;
 };
+
+console.log(factorial(10));
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
