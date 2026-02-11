6a3a45820312cfab6623cd9b77985918dac9377f
leo <187560246+Leodata01@users.noreply.github.com>
2026-02-11 13:58:57 +0700

    finish ex 14 fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let first = 0;
+  let second = 1;
+  let actual;
 
+  for (i = 2; i < n; i++) {
+    actual = first + second;
+    first = second;
+    second = actual;
+  }
 };
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
