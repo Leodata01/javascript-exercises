b4d0f5bfba103d8fc00c5a92f27a1de8207f994c
leo <187560246+Leodata01@users.noreply.github.com>
2026-02-11 13:15:28 +0700

    finish ex 13 palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string = "") {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+  const cleanedString = string
+    .toLocaleLowerCase()
+    .split("")
+    .filter((item) => alphanumerical.includes(item))
+    .join("");
+
+  const reversedString = cleanedString.split("").reverse().join("");
+
+  return cleanedString === reversedString;
 };
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
